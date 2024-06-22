b8f50954f18e9b5b7c9e55ed7a49dcd7a063648d
Yonatan Haddish <yonihailshn@gmail.com>
2024-06-21 23:25:56 -0400

    navbar structured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes></Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar-parent">
+        <div className="navbar-logo">
+          <h3>Logo</h3>
+        </div>
+        <div className="navbar-list">
+          <div className="link-home" id="navlink-item">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="link-about" id="navlink-item">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="link-portfolio" id="navlink-item">
+            <Link to="/portfolio">Portfolio</Link>
+          </div>
+          <div className="link-resume" id="navlink-item">
+            <Link to="/resume">Resume</Link>
+          </div>
+          <div className="link-contact" id="navlink-item">
+            <Link to="/contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Navbar;
