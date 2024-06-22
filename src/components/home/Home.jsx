e36939393ce9b01d51ad6c34f800801f493b7ca2
Yonatan Haddish <yonihailshn@gmail.com>
2024-06-22 16:03:45 -0400

    Home page structured

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "./index.css";
+import image from "../../assets/profilePhoto.png";
 function Home() {
-  return <div>Home</div>;
+  const imageStyle = {
+    height: "100%",
+  };
+  return (
+    <div className="home-parent">
+      <div className="text-buttons">
+        <div className="text-parent">
+          <h3>HELLO!</h3>
+          <h1>I am Yonatan Haddish.</h1>
+          <h2>A Full Stack Developer</h2>
+        </div>
+        <div className="button-parent">
+          <button>Hire Me</button>
+          <button>My Works</button>
+        </div>
+      </div>
+      <div className="image-box">
+        <img src={image} style={imageStyle}></img>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
