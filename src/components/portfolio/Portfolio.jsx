28fef12e8e7d5d30ad8f8a28aabf2fff8e405c68
Yonatan Haddish <yonihailshn@gmail.com>
2024-06-27 15:49:06 -0400

    Portfolio Page under-construction message

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./index.css";
 
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className="portfolio-parent">
+      <h1>Under construction</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
 }
 
 export default Portfolio;
