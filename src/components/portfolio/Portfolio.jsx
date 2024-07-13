4e4037eac2e09ff905550e5595c6ac786c5b8a62
Yonatan Haddish <yonihailshn@gmail.com>
2024-07-13 14:12:59 -0400

    Portfolio page structured

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,25 @@ import "./index.css";
 function Portfolio() {
   return (
     <div className="portfolio-parent">
-      <h1>Under construction</h1>
-      <p>Coming soon...</p>
+      <h1>Portfolio</h1>
+      <div className="portfolio-list">
+        <div className="portfolio-list-one">
+          <div className="card-01" id="card">
+            MetaFlix
+          </div>
+          <div className="care-02" id="card">
+            Network Automation
+          </div>
+        </div>
+        <div className="portfolio-list-two">
+          <div className="card-03" id="card">
+            Alpha Stream
+          </div>
+          <div className="care-04" id="card">
+            XYZ Car Dealer
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
