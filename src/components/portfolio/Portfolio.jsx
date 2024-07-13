9d6842df37d4a1f75c2b16fc981a66a3aa2e66d5
Yonatan Haddish <yonihailshn@gmail.com>
2024-07-13 14:28:39 -0400

    portfolio page styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,21 +6,17 @@ function Portfolio() {
     <div className="portfolio-parent">
       <h1>Portfolio</h1>
       <div className="portfolio-list">
-        <div className="portfolio-list-one">
-          <div className="card-01" id="card">
-            MetaFlix
-          </div>
-          <div className="care-02" id="card">
-            Network Automation
-          </div>
+        <div className="card-01" id="card">
+          MetaFlix
         </div>
-        <div className="portfolio-list-two">
-          <div className="card-03" id="card">
-            Alpha Stream
-          </div>
-          <div className="care-04" id="card">
-            XYZ Car Dealer
-          </div>
+        <div className="care-02" id="card">
+          Network Automation
+        </div>
+        <div className="card-03" id="card">
+          Alpha Stream
+        </div>
+        <div className="care-04" id="card">
+          XYZ Car Dealer
         </div>
       </div>
     </div>
