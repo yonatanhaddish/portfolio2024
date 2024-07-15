17806cad2aa40b04cce6c3fe6a6cf1ead5427a13
Yonatan Haddish <yonihailshn@gmail.com>
2024-07-15 15:38:55 -0400

    selected single project structure and implemented

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 function Portfolio() {
+  const [selectedSingleProject, setSelectedSingleProject] = useState("");
+
+  const portfolio_obj = [
+    {
+      name: "MetaFlix",
+      url_link: `metaflix.test.com`,
+    },
+    {
+      name: "Network Automation",
+      url_link: `network_automation.test.com`,
+    },
+    {
+      name: "Alpha Stream",
+      url_link: `alpha_stream.test.com`,
+    },
+    {
+      name: "XYZ Car Dealership",
+      url_link: `xyz_cardealership.test.com`,
+    },
+  ];
+
+  function handleSelectedProject(event) {
+    setSelectedSingleProject(event.target.innerHTML);
+  }
+
+  console.log(selectedSingleProject);
   return (
     <div className="portfolio-parent">
       <h1>Portfolio</h1>
-      <div className="portfolio-list">
+      {/* <div className="portfolio-list">
         <div className="card-01" id="card">
           MetaFlix
         </div>
-        <div className="care-02" id="card">
+        <div className="card-02" id="card">
           Network Automation
         </div>
         <div className="card-03" id="card">
           Alpha Stream
         </div>
-        <div className="care-04" id="card">
+        <div className="card-04" id="card">
           XYZ Car Dealer
         </div>
+      </div> */}
+      <div className="portfolio-list">
+        {portfolio_obj.map((item, index) => (
+          <div
+            id="card"
+            key={index}
+            value={selectedSingleProject}
+            onClick={handleSelectedProject}
+          >
+            {item.name}
+          </div>
+        ))}
       </div>
     </div>
   );
