97d185db077c08ef530f46a18a0ab84ba30a5e0c
Yonatan Haddish <yonihailshn@gmail.com>
2024-08-28 19:53:29 -0400

    deploy testing

--- src/components/utils/BasicModal.jsx
+++ src/components/utils/BasicModal.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import "./index.css";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { CiLink } from "react-icons/ci";
